940faea2b40ef5c5ebdab18a90a1fc012fd1b878
thayron <thayronf3@gmail.com>
2020-12-04 02:35:14 -0300

    Refatorando a tela de login em componentes para ser reaproveitado em outros pages

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Button from '../../components/button/index';
+import Input from '../../components/input/index';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -9,9 +11,9 @@ const SignIn: React.FC = () => (
       <img src={logoImg} alt="GoBarber" />
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit"> Entrar </button>
+        <Input name="E-mail" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+        <Button name="button" type="submit"> Entrar </Button>
         <a href="forgot"> Esqueci minha senha </a>
       </form>
       <a href="login">

27f7ba7400848259ef5e31223643776758441d4c
thayron <thayronf3@gmail.com>
2020-12-04 10:23:25 -0300

    Foi usado as bibliotecas @unform/core @unform/web para usar no lugar do form html

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import {
+  FiLogIn, FiMail, FiUser, FiLock,
+} from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
+
 import Button from '../../components/button/index';
 import Input from '../../components/input/index';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Faça o seu cadastro</h1>
-        <Input name="Name" icon={FiUser} placeholder="Nome" />
-        <Input name="E-mail" icon={FiMail} placeholder="E-mail" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-        <Button type="submit"> Cadastrar </Button>
-      </form>
-      <a href="login">
-        <FiLogIn />
-        Voltar para logon
-      </a>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form initialData={{ name: 'thayron' }} onSubmit={handleSubmit}>
+          <h1>  Faça o seu cadastro </h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Button type="submit"> Cadastrar </Button>
+        </Form>
+        <a href="login">
+          <FiLogIn />
+          Voltar para logon
+        </a>
+
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
